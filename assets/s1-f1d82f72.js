bd389ddc776943c21f5cce0e5e4c14f56996b6ae
DESKTOP-5E76NPE\tiger <1909446527@qq.com>
2024-11-25 15:03:03 +0800

    +++

--- assets/s1-f1d82f72.js
+++ assets/s1-f1d82f72.js
@@ -1,1 +1,1 @@
-const s="/cola-designer-pro-doc/assets/s1-9bfc1b3d.png";export{s as _};
+const s="/assets/s1-9bfc1b3d.png";export{s as _};
--- assets/s5-177e486e.js
+++ assets/s5-177e486e.js
@@ -1,1 +1,1 @@
-const s="/cola-designer-pro-doc/assets/s5-34cb0c5e.png";export{s as _};
+const s="/assets/s5-34cb0c5e.png";export{s as _};
